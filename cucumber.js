8737888af7f59cf3c9b65c2e0b2e44f8e49328dc
Sandor Simon <simsany@gmail.com>
2022-11-07 13:58:31 +0100

    refactor cucumber config

--- cucumber.js
+++ cucumber.js
@@ -1,15 +1,3 @@
-// module.exports = {
-//   default: [
-//     "--require-module ts-node/register",
-//     "./test/features/*.feature",
-//     "--require test/step_definitions/*.ts",
-//     "--parallel 2",
-//     "--format progress-bar",
-//     "--format json:test/reports/json-test-report.json",
-//     "--format html:test/reports/html-test-report.html",
-//   ].join(" "),
-// };
-
 module.exports = {
   default: {
     requireModule:["ts-node/register"],
