b563e59bdb8d4f79f2421db5e68f7a070ff85a04
Sandor Simon <simsany@gmail.com>
2022-11-07 13:57:45 +0100

    fix reporter file not found error

--- cucumber.js
+++ cucumber.js
@@ -1,11 +1,22 @@
+// module.exports = {
+//   default: [
+//     "--require-module ts-node/register",
+//     "./test/features/*.feature",
+//     "--require test/step_definitions/*.ts",
+//     "--parallel 2",
+//     "--format progress-bar",
+//     "--format json:test/reports/json-test-report.json",
+//     "--format html:test/reports/html-test-report.html",
+//   ].join(" "),
+// };
+
 module.exports = {
-  default: [
-    "--require-module ts-node/register",
-    "./test/features/*.feature",
-    "--require test/step_definitions/*.ts",
-    "--parallel 2",
-    "--format progress-bar",
-    "--format json:test/reports/cucumber-report.json",
-    "--format html:test/reports/cucumber-report.html",
-  ].join(" "),
+  default: {
+    requireModule:["ts-node/register"],
+    paths:["./test/features/*.feature"],
+    require:["test/step_definitions/*.ts"],
+    parallel: 2,
+    format: ["progress-bar","json:test/reports/json-test-report.json", "html:test/reports/html-test-report.html"]
+   
+  }
 };
